62c6d62277222140c47864537280ae8eb4b49242
Will Roscoe <wroscoe@gmail.com>
2018-01-05 07:13:24 +0000

    adding robo4j libs

--- management/src/main/groovy/galecino/VehicleService.groovy
+++ management/src/main/groovy/galecino/VehicleService.groovy
@@ -3,7 +3,17 @@ package galecino
 import grails.gorm.services.Service
 
 @Service(Vehicle)
-interface VehicleService {
-    List<Vehicle> list()
-    Vehicle save(String name)
+abstract class VehicleService {
+
+
+    abstract List<Vehicle> list()
+    abstract Vehicle save(String name)
+    abstract pwmTest() {
+           PWMPCA9685Device device = new PWMPCA9685Device()
+           PCA9685Servo servo0 = device.getChannel(0)
+           device.setPWMFrequency(50)
+           servo0.setPWM(0, 205);
+           PWMChannel servo0 = device.getChannel(0) 
+                    
+    }
 }
