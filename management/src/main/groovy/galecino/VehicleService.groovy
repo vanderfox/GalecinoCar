84d7f27f8d04e267123e2c912035a678148ab0ce
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2018-01-07 00:59:22 -0600

    robo4j fun

--- management/src/main/groovy/galecino/VehicleService.groovy
+++ management/src/main/groovy/galecino/VehicleService.groovy
@@ -1,5 +1,6 @@
 package galecino
 
+import com.robo4j.hw.rpi.Servo
 import com.robo4j.hw.rpi.i2c.pwm.PCA9685Servo
 import com.robo4j.hw.rpi.i2c.pwm.PWMPCA9685Device
 import grails.gorm.services.Service
@@ -7,14 +8,48 @@ import grails.gorm.services.Service
 @Service(Vehicle)
 abstract class VehicleService {
 
+    private static final int SERVO_FREQUENCY = 50;
+    private static final int MOTOR_MIN = 0;
+    private static final int MOTOR_MEDIUM = 2048;
+    private static final int MOTOR_MAX = 4095;
 
     abstract List<Vehicle> list()
     abstract Vehicle save(String name)
     void pwmTest() {
-           PWMPCA9685Device device = new PWMPCA9685Device()
-           PCA9685Servo servo0 = device.getChannel(0)
-           device.setPWMFrequency(50)
-           servo0.setPWM(0, 205)
+        System.out.println("Creating device...");
+        PWMPCA9685Device device = new PWMPCA9685Device();
+        device.setPWMFrequency(SERVO_FREQUENCY);
+        Servo servo0 = new PCA9685Servo(device.getChannel(1));
+        //Servo servo1 = new PCA9685Servo(device.getChannel(1));
+        PWMPCA9685Device.PWMChannel motor0 = device.getChannel(0);
+        //PWMPCA9685Device.PWMChannel motor1 = device.getChannel(3);
 
+        System.out.println("Setting start conditions...");
+        servo0.setInput(0);
+        //servo1.setInput(0);
+        motor0.setPWM(0, MOTOR_MIN);
+        //motor1.setPWM(0, MOTOR_MIN);
+
+        System.out.println("Press enter to run loop!");
+        System.in.read();
+        System.out.println("Running perpetual loop...");
+        while (true) {
+            servo0.setInput(-1);
+            //servo1.setInput(-1);
+            motor0.setPWM(0, MOTOR_MEDIUM);
+            //motor1.setPWM(0, MOTOR_MEDIUM);
+            Thread.sleep(500);
+            servo0.setInput(1);;
+            //servo1.setInput(1);;
+            motor0.setPWM(0, MOTOR_MAX);
+            //motor1.setPWM(0, MOTOR_MAX);
+            Thread.sleep(500);
+            servo0.setInput(0);
+            //servo1.setInput(0);
+            motor0.setPWM(0, MOTOR_MIN);
+            //motor1.setPWM(0, MOTOR_MIN);
+            Thread.sleep(1000);
+        }
     }
+
 }
